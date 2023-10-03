35daa3633e90492b34e36149e8685a1a915654a3
Alefga <floregaston41@gmail.com>
2023-10-03 12:03:28 +0200

    Ejercicio 2

---
+++ untitled4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 11:57:08 2023
+
+@author: gflogar
+"""
+
+a=int(input("Dime un número"))
+b=int(input("Dime un número"))
+if a < b:
+    print ("El valor menor es:", a)
+else:
+    print ("El valor menor es:", b)
