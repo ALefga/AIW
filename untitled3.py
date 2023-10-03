673b74e1e09d1e075b4c2336254f4a499f44174e
Alefga <floregaston41@gmail.com>
2023-10-03 11:11:10 +0200

    Ejercicio 1

---
+++ untitled3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 11:03:44 2023
+
+@author: gflogar
+"""
+
+a=int(input("Dime un número"))
+if a == 1000: 
+    print("GANASTE UN PREMIO")
+else:
+    print("sigue intentando")
