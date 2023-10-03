4f3504c93f346e5aab73fce027de144ef7f57ceb
Alefga <floregaston41@gmail.com>
2023-10-03 12:34:44 +0200

    ejercicio 6

---
+++ untitled8.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 12:28:07 2023
+
+@author: gflogar
+"""
+
+num = int(input("dime un numero entero:"))
+if num % 2 == 0:
+    print("es un numero par")
+else:
+    print("no es un numero par")
