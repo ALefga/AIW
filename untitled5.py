7d51d7d248af969110f110d9b1e72d45f81fbe8d
Alefga <floregaston41@gmail.com>
2023-10-03 12:07:23 +0200

    ejercicio 3

---
+++ untitled5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 12:03:56 2023
+
+@author: gflogar
+"""
+
+a=int(input("Dime un número"))
+b=int(input("Dime un número"))
+if a < b:
+    print ("El valor menor es:", a)
+elif b < a:
+    print ("El valor menor es:", b)
+else:
+    print ("son iguales")
