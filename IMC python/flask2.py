8b7fee9e22234d0477af709894c5316b3c75912e
Alefga <floregaston41@gmail.com>
2023-11-14 11:11:25 +0100

    IMC python

--- IMC python/flask2.py
+++ IMC python/flask2.py
@@ -2,22 +2,38 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST","GET"])
-def hola():
-    if (request.method == "POST"):
-        weight = request.form.get("Peso")
-        height = request.form.get("Altura")
-        
-        # Validar que los datos sean numéricos y positivos
-        if weight.isnumeric() and height.isnumeric() and float(weight) > 0 and float(height) > 0:
-            print(weight + height)
-        else:
-            # Si los datos no son válidos, mostrar un mensaje de error
-            return render_template("error.html", message="Los datos ingresados no son válidos")
-    return render_template("index1.html")
-app.run()
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
 
-"""menor de 18.5 bajo peso
+def clasificar_imc(imc):
+    if imc < 18.5:
+        return "Bajo peso"
+    elif imc >= 18.5 and imc <= 24.9:
+        return "Normal"
+    elif imc >= 25 and imc <= 29.9:
+        return "Sobrepeso"
+    else:
+        return "Obesidad"
+
+@app.route("/IMC", methods=["POST","GET"])
+def calcular_imc_flask():
+    if request.method == "POST":
+        peso = float(request.form.get("Peso"))
+        altura = float(request.form.get("Altura"))
+
+        imc = calcular_imc(peso, altura)
+        clasificacion = clasificar_imc(imc)
+
+        return render_template("index1.html", imc=imc, clasificacion=clasificacion)
+    else:
+        return render_template("index1.html")
+
+if __name__ == "__main__":
+    app.run()
+
+"""
+menor de 18.5 bajo peso
 18.5 y 24.9 normal
 25 y 29.9 sobrepeso
 mayor de 30 obesidad
