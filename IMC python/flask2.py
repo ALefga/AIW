4dd7795c7b520a23e46702e3a4d161ed8e150670
Alefga <floregaston41@gmail.com>
2023-11-14 11:53:00 +0100

    IMC python

--- IMC python/flask2.py
+++ IMC python/flask2.py
@@ -2,28 +2,22 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-def calcular_imc(peso, altura):
-    imc = peso / (altura ** 2)
-    return imc
-
-def clasificar_imc(imc):
-    if imc < 18.5:
-        return "Bajo peso"
-    elif imc >= 18.5 and imc <= 24.9:
-        return "Normal"
-    elif imc >= 25 and imc <= 29.9:
-        return "Sobrepeso"
-    else:
-        return "Obesidad"
-
 @app.route("/IMC", methods=["POST","GET"])
 def calcular_imc_flask():
     if request.method == "POST":
         peso = float(request.form.get("Peso"))
         altura = float(request.form.get("Altura"))
 
-        imc = calcular_imc(peso, altura)
-        clasificacion = clasificar_imc(imc)
+        imc = peso / (altura ** 2)
+
+        if imc < 18.5:
+            clasificacion = "Bajo peso"
+        elif imc >= 18.5 and imc <= 24.9:
+            clasificacion = "Normal"
+        elif imc >= 25 and imc <= 29.9:
+            clasificacion = "Sobrepeso"
+        else:
+            clasificacion = "Obesidad"
 
         return render_template("index1.html", imc=imc, clasificacion=clasificacion)
     else:
@@ -31,7 +25,6 @@ def calcular_imc_flask():
 
 if __name__ == "__main__":
     app.run()
-
 """
 menor de 18.5 bajo peso
 18.5 y 24.9 normal
